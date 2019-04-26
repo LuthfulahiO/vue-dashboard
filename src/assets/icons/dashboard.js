027167495848522f831297d7645620f0b8cc7c9f
LuthfulahiO <luteeph@gmail.com>
2019-04-26 11:41:47 +0100

    update: clean up :lipstick:

--- src/assets/icons/dashboard.js
+++ src/assets/icons/dashboard.js
@@ -1,10 +1,11 @@
 /* eslint-disable */
-var icon = require('vue-svgicon')
+var icon = require('vue-svgicon');
 icon.register({
-  'baseline-dashboard-24px': {
+  dashboard: {
     width: 24,
     height: 24,
     viewBox: '0 0 24 24',
-    data: '<path pid="0" d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z" _fill="#fff"/>'
-  }
-})
+    data:
+      '<path pid="0" d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z" _fill="#fff"/>',
+  },
+});
